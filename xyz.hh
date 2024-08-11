bb57c38453aeea472e1457c2348d56b715126e46
apuiji <2330683684@qq.com>
2024-08-11 16:51:49 +0800

    a

--- xyz.hh
+++ xyz.hh
@@ -1,38 +1,46 @@
 #pragma once
 
+#include<concepts>
 #include<utility>
 
 namespace zlt {
   template<class ...T>
-  struct Dynamicastable {
-    template<class U>
-    requires (std::is_pointer_v<U>)
-    bool operator ()(U u) const noexcept {
-      return (dynamic_cast<const T *>(u) || ...);
-    }
-    template<class U>
-    requires (!std::is_pointer_v<U>)
-    bool operator ()(U &&u) const noexcept {
-      return operator ()(&u);
+  struct Types {};
+
+  template<class T, class ...U>
+  static inline bool dynamicastable(const T *t, Types<U...>) noexcept {
+    return (dynamic_cast<const U *>(t) || ...);
+  }
+
+  template<class T, class ...U>
+  requires (!std::is_pointer_v<T>)
+  static inline bool dynamicastable(const T &t, Types<U...> us) noexcept {
+    return dynamicastable(&t, us);
+  }
+
+  template<std::invocable DoSth>
+  struct Guard {
+    DoSth doSth;
+    Guard(const DoSth &doSth) noexcept: doSth(doSth) {}
+    Guard(DoSth &&doSth) noexcept: doSth(std::move(doSth)) {}
+    ~Guard() {
+      doSth();
     }
   };
 
-  template<class ...T>
-  struct Types {};
-
   template<class T>
-  static inline constexpr T initializerListGet(std::initializer_list<T> ts, size_t i) noexcept {
+  static inline constexpr T initialistGet(std::initializer_list<T> ts, size_t i, T elze = T()) noexcept {
     for (T t : ts) {
       if (!i) {
         return t;
       }
       --i;
     }
-    return T();
+    return elze;
   }
 
   template<class T, class U>
-  static inline constexpr int initializerListIndexOf(std::initializer_list<T> ts, U u) noexcept {
+  static inline constexpr int initialistIndexOf(std::initializer_list<T> ts, U u) noexcept {
     int i = 0;
     for (T t : ts) {
       if (t == u) {
